022b7ba74989803f73040295aab43821714006a2
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-04-24 10:41:13 +0300

    Add files via upload

---
+++ ProductsApp/ApiUI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ApiUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //HttpClient cl = new HttpClient();
+            //cl.BaseAddress = new Uri("http://localhost:61342/product");
+            //HttpResponseMessage response = cl.GetAsync("/getProducts").Result;
+
+            //var pr = response.Content.ReadAsAsync<List<Product>>().Result;
+            ////var pr = response.Content.ReadAsStringAsync().Result;
+            //dataGridView1.DataSource = pr;
+
+            GetAllProducts();
+
+        }
+
+        private async void GetAllProducts()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("http://localhost:61342/product/getProducts"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var productJsonString = await response.Content.ReadAsStringAsync();
+
+                        dataGridView1.DataSource = JsonConvert.DeserializeObject<Product>(productJsonString);
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProductsApp/ApiUI/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUI
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember(Name="id")]
+        public int Id { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "category")]
+        public string Category { get; set; }
+        [DataMember(Name = "price")]
+        public decimal Price { get; set; }
+    }
+}
